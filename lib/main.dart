dc2d1dbaaf7eb5a2ff029ecb9da37008e998e44b
Pepe Valdivia <pepe.valdivia.caballer@gmail.com>
2026-04-13 22:06:50 -0500

    aplicando colores y tema

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+// main.dart
 import 'package:flutter/material.dart';
+import './configs/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +12,39 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final TextTheme baseTextTheme = Typography.material2021().englishLike;
+    final MaterialTheme materialTheme = MaterialTheme(baseTextTheme);
+    final colors = Theme.of(context).colorScheme;
+
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
+    print('1 ++++++++++++++++++++++++++++++++');
+
+
+    ThemeData myLightTheme;
+
+    if(isDarkMode){
+       myLightTheme = materialTheme.dark();
+    }else{
+       myLightTheme = materialTheme.light();
+    }
+
     return MaterialApp(
       title: 'Flutter Demo',
+      theme: materialTheme.light(),
+      darkTheme: materialTheme.dark(),
       home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Biblio App UL", 
+            style: TextStyle(
+                color: colors.primary
+              ),
+            ),
+          backgroundColor: myLightTheme.colorScheme.tertiaryContainer
+        ),
         body: Center(
-          child: Text('hola mundo')
+          child: Text("hola mundo")
         ),
       ),
     );
